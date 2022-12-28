e17de45e7fd9fd6c08f15ef1955c4de3aa1b387a
danangkraton <kratondanang@gmail.com>
2022-12-28 20:49:15 +0700

    add database

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask, request
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
 from flask_restful import Resource, Api
 from flask_cors import CORS
 import random 
+from pymongo import MongoClient
+from send_data import send_data
+
+CONNECTION_STRING = "mongodb://danangwahyu:Semangat45@ac-d7hxbx4-shard-00-00.u7vhogz.mongodb.net:27017,ac-d7hxbx4-shard-00-01.u7vhogz.mongodb.net:27017,ac-d7hxbx4-shard-00-02.u7vhogz.mongodb.net:27017/?ssl=true&replicaSet=atlas-n0tl0g-shard-0&authSource=admin&retryWrites=true&w=majority"
+
+client = MongoClient(CONNECTION_STRING)
+dbname = client["vital_signs"]
+db_ir = dbname["raw_ir"]
+db_bp = dbname["blood_pressure"]
+db_heat = dbname["heat_body"]
+db_heart = dbname["heart_rate"]
+db_oksi = dbname["oksi"]
+
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "f9cfa0be9fc687395abc02588cba4ae3efb4c496"
-# app.config["MONGO_URI"] = "mongodb+srv://danangwahyu:Semangat45@cluster0.u7vhogz.mongodb.net/?retryWrites=true&w=majority"
-app.config["MONGO_URI"] = "mongodb://danangwahyu:Semangat45@ac-d7hxbx4-shard-00-00.u7vhogz.mongodb.net:27017,ac-d7hxbx4-shard-00-01.u7vhogz.mongodb.net:27017,ac-d7hxbx4-shard-00-02.u7vhogz.mongodb.net:27017/?ssl=true&replicaSet=atlas-n0tl0g-shard-0&authSource=admin&retryWrites=true&w=majority"
-
-# mongodb database
-mongodb_client = PyMongo(app)
-db = mongodb_client.db
 
 api = Api(app)
 CORS(app)
@@ -29,6 +35,17 @@ class add_data(Resource):
         data_vital_signs["data_oksi"] = data_oksi
         data_vital_signs["data_IR"] = data_IR
         data_vital_signs["data_bp"] = data_IR**2
+        db_ir.insert_one(send_data("raw_ir", data_IR))
+        db_bp.insert_one(send_data("blood_pressure", data_IR**2))
+        db_heat.insert_one(send_data("heat_body", data_heat))
+        db_heart.insert_one(send_data("heart_rate", data_heart))
+        db_oksi.insert_one(send_data("oksi", data_oksi))
+
+
+        # db_bp.insert_one({"data": data_IR**2})
+        # db_heat.insert_one({"data": data_heat})
+        # db_heart.insert_one({"data": data_heart})
+        # db_oksi.insert_one({"data": data_oksi})
         return {"msg": "Data tersimpan",
         "data": data_vital_signs}
 
@@ -53,6 +70,9 @@ api.add_resource(bp, "/bp", methods = ["GET"])
 api.add_resource(heat, "/heat", methods = ["GET"])
 api.add_resource(oksi, "/oksi", methods = ["GET"])
 api.add_resource(heart, "/heart", methods = ["GET"])
+
+
+
 
 if __name__ =="__main__":
     app.run(debug=True)
---
+++ send_data.py
@@ -0,0 +1,6 @@
+def send_data(jenis_data, data):
+    return {
+        "jenis_data" : jenis_data,
+        "data": data,
+        "time" : "waktu"
+    }
